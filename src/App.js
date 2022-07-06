03c1f3c7f3509c6ec513ca26e170fb4969dc76f5
potnuruavinash <potnuruavinash111@gmail.com>
2022-07-06 12:19:27 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
 
-function App() {
-  return (
+import "./App.css"
+
+
+const App=()=>{
+  const [input,setInput]=useState("")
+  const [result,setResult]=useState("0")
+  const handle=e=>{
+    setInput(e.target.value)
+  }
+  const clean=()=>{
+    setInput("")
+    setResult("0")
+  }
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="mb-3">Calculator</h1>
+      <input value={input} name="input" type="text" onChange={handle} />
+      <br/>
+         
+      <button onClick={()=>setResult(eval(input))} className=" btn btn-primary ">calculate</button>
+      <h3>Result is: {result}</h3>
+      <center className="mt-3">
+        <button  onClick={()=>setInput(input+"1")} >1</button>
+        <button  onClick={()=>setInput(input+"2")}>2</button>
+        <button   onClick={()=>setInput(input+"3")}>3</button>
+         <button  onClick={()=>setInput(input+"4")}>4</button>
+        
+         <button  onClick={()=>setInput(input+"5")}>5</button>
+         <br/>
+       <button  onClick={()=>setInput(input+"6")}>6</button>
+        <button  onClick={()=>setInput(input+"7")}>7</button>
+         <button  onClick={()=>setInput(input+"8")}>8</button>
+        
+          <button  onClick={()=>setInput(input+"9")}>9</button>
+          <button  onClick={()=>setInput(input+"0")}>0</button>
+          <br/>
+       <button  onClick={()=>setInput(input+"+")}>+</button>
+        <button  onClick={()=>setInput(input+"-")}>-</button>
+         <button  onClick={()=>setInput(input+"*")}>*</button>
+         <button  onClick={()=>setInput(input+"/")}>/</button>
+         <button  onClick={clean}>clr</button>
+
+      </center>
+    
+     
     </div>
-  );
+  )
+
 }
 
 export default App;
